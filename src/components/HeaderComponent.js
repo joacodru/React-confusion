1f97156ab9274580014c03ae8cd9310d939fc001
joacodru <joadruetta@gmail.com>
2020-12-03 10:57:11 -0300

    Comment form added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -5,6 +5,7 @@ import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
 import { NavLink } from 'react-router-dom';
 
 
+
 class Header extends Component {
     
     constructor(props) {
